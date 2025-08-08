a1f3ec5d8cd4049a5403662a693e12d8a5bb3fe2
Avdi Iseni <avdiseni07@gmail.com>
2025-08-08 10:35:13 +0200

    Finished generating problems based on prompts (categories) and storing them as needed.

--- CustomDSATrainer/AIService/main.py
+++ CustomDSATrainer/AIService/main.py
@@ -8,14 +8,14 @@ gemini_api_key = os.getenv("API_KEY")
 client = genai.Client(api_key = gemini_api_key)
 question = ""
 
-with open('LLMPrompt.txt', 'r') as file:
+with open('LLMPrompt.txt', 'r', encoding='utf-8') as file:
     question = file.read()
 
 response = client.models.generate_content(
     model = "gemini-2.0-flash", contents = question
 )
 
-#print(response.text)
+print(response.text)
 lines = response.text.splitlines()
 
 inputStarted = False
@@ -26,6 +26,7 @@ input = ""
 output = ""
 
 currentInput = 1
+currentOutput = 1
 
 for line in lines:
     if inputStarted == False:
@@ -38,6 +39,9 @@ for line in lines:
     elif outputStarted == False:
         if line == 'OUTPUT':
             outputStarted = True
+            nameOfFile = "Task/Inputs/" + str(currentInput)
+            with open(nameOfFile, 'w') as f:
+                f.write(input)
             continue
         else:
             if line == '!':
@@ -50,14 +54,19 @@ for line in lines:
                 input += line
                 input += '\n'
     else:
-        output += line
-        input += '\n'
+        if line == '!':
+            nameOfFile = "Task/Outputs/" + str(currentOutput)
+            with open(nameOfFile, 'w') as f:
+                    f.write(output)
+                    output = ""
+            currentOutput += 1
+        else:
+            output += line
+            output += '\n'
+
+nameOfFile = "Task/Outputs/" + str(currentOutput)
+with open(nameOfFile, 'w') as f:
+    f.write(output)
         
 with open('Task/Statement.txt', 'w') as statementFile:
     statementFile.write(statement)
-
-with open('Task/InputNumbers.txt', 'w') as inputFile:
-    inputFile.write(input)
-    
-with open('Task/ExpectedOutput.txt', 'w') as outputFile:
-    outputFile.write(output)
