5a2b3d86664087823e0d4fac981d6ba825506a70
Avdi Iseni <avdiseni07@gmail.com>
2025-08-08 11:47:35 +0200

    Running the python script from within .Net

---
+++ CustomDSATrainer/Application/PythonAIService.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Core.Pipeline;
+using System.Diagnostics;
+
+namespace CustomDSATrainer.Application
+{
+    public static class PythonAIService
+    {
+        public static string pathToPython = "AIService/main.py";
+
+        private static void RunPythonScript()
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "python",
+                Arguments = $"-u \"{Path.GetFullPath(pathToPython)}\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            };
+
+            var process = new Process { StartInfo = startInfo };
+            process.Start();
+        }
+
+    }
+}
